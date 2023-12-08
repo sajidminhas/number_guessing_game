0e707d28c02fc994453b2163f01a29192e03b3e1
Sajid Minhas <sajidminhas@gmail.com>
2023-12-08 20:38:07 +0100

    added functionality to make 10 attempts to play.

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 9;
 
 function checkGuess() {
   const inputElement = document.getElementById('guess');
   const feedbackElement = document.getElementById('feedback');
-  let guess = Number(inputElement.value);
-  if (guess === randomNumber) {
-    feedbackElement.innerHTML = "Wow you are right!";
-    feedbackElement.style.color = "green"; // Success color
-  } else if (guess < randomNumber) {
-    feedbackElement.innerHTML = "Too low! Try again";
-    feedbackElement.style.color = "red";
-  } else if (guess > randomNumber) {
-    feedbackElement.innerHTML = "Too high! Try again.";
-    feedbackElement.style.color = "red";
-  } else {
-    feedbackElement.innerHTML = "Please enter a valid number.";
+  while (attempts >= 0) {
+    let guess = Number(inputElement.value);
+    if (guess === randomNumber) {
+      attempts = 0;
+      feedbackElement.innerHTML = "Wow you are right!";
+      feedbackElement.style.color = "green"; // Success color
+      break;
+    } else if (guess < randomNumber) {
+      feedbackElement.innerHTML =
+        `Too low! Try again! <br> number of attempt left ${attempts}`;
+      feedbackElement.style.color = "red";
+      break;
+    } else if (guess > randomNumber) {
+      feedbackElement.innerHTML =
+        `Too high! Try again! <br> number of attempt left ${attempts}`;
+      feedbackElement.style.color = "red";
+      break;
+    } else {
+      feedbackElement.innerHTML = "Please enter a valid number.";
+      feedbackElement.style.color = "red";
+      break;
+    }
+  }
+  if (attempts === 0 && guess !== randomNumber) {
+    feedbackElement.innerHTML =
+      "You could'not guess the number <br> *** Game Over ****";
     feedbackElement.style.color = "red";
   }
+  attempts -= 1;
 }
